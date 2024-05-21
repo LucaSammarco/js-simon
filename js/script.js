8072fc1bfb6f7b0b4bf4c5239bcf6ac0d59ed858
Il tuo nome <la_tua_email@example.com>
2024-05-21 16:06:11 +0200

    added unique number generator

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
 
 let numeriGenerati = []
 
+const numeriDaGenerare = 5
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max + 1);
   }
 
-  for (let index = 1; index <= 5; index++) {
+//   for (let index = 1; index <= 5; index++) {
+//     let numero = getRandomInt(10);
+//     numeriGenerati.push(numero)
+    
+//   }
+
+// while (numeriGenerati.length < 6) {
+//     let index = []
+// }
+
+
+// function generateUniqueNumbers(numeriDaGenerare) {
+//     let numero = [];
+//     while (numeriGenerati.length < numeriDaGenerare) {
+//         let  = getRandomInt(10);
+//         if (!bombsCells.includes(bombNum)) {
+//             bombsCells.push(bombNum);
+//         }
+//     }
+//     return bombsCells;
+// }
+
+
+  while (numeriGenerati.length <= numeriDaGenerare - 1 ) {
+    
     let numero = getRandomInt(10);
-    numeriGenerati.push(numero)
+        if (!numeriGenerati.includes(numero)) {
+            numeriGenerati.push(numero)
+        }
     
   }
+
+
+//   while (condition) {
+    
+//   }
